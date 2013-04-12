39097b9014bda7a07aa1951ffd75a83b1a31033e
kevin Newesil <kevin@sourjelly.net>
2013-04-12 15:36:16 +0200

    bugfix
    bugfix in ifstatement. forget a few ()

--- core/build/sourjelly.class.php
+++ core/build/sourjelly.class.php
@@ -103,7 +103,7 @@
 					self::$_html = new HtmlBase('main');
 
 				//Check for premission again so that administrators don't have to be the only users on the website, and login is made possible.
-				if(isset($_SESSION['login']) || isset($_GET['login']) && $_GET['login'] == 'login' || isset($_POST['login']) || \api\Api::getUsers() -> getUserPremissionBySession() > 1 || $fun[0] == 'auth' && !isset($fun[1]) || $fun[1] == '')
+				if(isset($_SESSION['login']) || isset($_GET['login']) && $_GET['login'] == 'login' || isset($_POST['login']) || \api\Api::getUsers() -> getUserPremissionBySession() > 1 || (isset($fun) && $fun[0] == 'auth' && (!isset($fun[1]) || $fun[1] == '')))
 					self::$_al = new \core\build\autoloader;
 				else
 					self::$_wv = new \core\build\Webview;
