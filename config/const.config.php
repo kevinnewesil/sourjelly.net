1728a3814ccaaca6d9143b8d3319fa5b1570d587
kevin Newesil <kevin@sourjelly.net>
2013-04-23 15:33:08 +0200

    ccommit

--- config/const.config.php
+++ config/const.config.php
@@ -13,16 +13,19 @@
 	if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] == 'localhost' && $_SERVER['SCRIPT_NAME'] !== 'index.php')
 	{
 		$dirname = dirname($_SERVER['SCRIPT_NAME']);
-
+		$dirname = str_replace('ajax/','public_html/',$dirname);
+		
 		DEFINE('AJAX_PATH', dirname($dirname) . DS . 'ajax' . DS);
-		DEFINE('ASSETS_PATH', str_replace('ajax/','public_html/', $dirname ) . DS . 'assets' . DS);
+		DEFINE('ASSETS_PATH', $dirname . DS . 'assets' . DS);
 		DEFINE('MODULES_ASSETS_PATH' ,dirname($dirname) . DS .  'modules' . DS);
 	}
 	else
     {
         $parts = explode('index.php' , $_SERVER['REQUEST_URI']);
+        $parts[0] = str_replace('ajax/','public_html/',$parts[0]);
+
 		DEFINE('AJAX_PATH' , MAIN_PATH . 'ajax' . DS);
-		DEFINE('ASSETS_PATH' , str_replace('ajax/','public_html/',$parts[0]) . 'assets' . DS);
+		DEFINE('ASSETS_PATH' , $parts[0] . 'assets' . DS);
 		DEFINE('MODULES_ASSETS_PATH' , '..' . DS . 'modules' . DS);
 	}
 
