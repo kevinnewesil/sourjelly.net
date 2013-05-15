a32fb28efb041a5c14d4f80c4879d9194a7f320a
kevin Newesil <kevin@sourjelly.net>
2013-05-15 14:58:37 +0200

    update on cli tool with colours, + start basic test files

--- core/helpers.php
+++ core/helpers.php
@@ -72,3 +72,64 @@
 			echo '</pre>';
 		}
 	}
+
+	//--------------------------------------------------------------------------------------------------------------------||
+	// Render colours into display
+	//--------------------------------------------------------------------------------------------------------------------||
+
+	if(!function_exists('colourString')){
+	// Function that parses string and return string with right colour values;
+		function colourString($string, $foregroundColour = NULL, $backgroundColour = NULL)
+		{
+			//--------------------------------------------------------------------------------------------------------------------||
+			// Predefine array with ansi colour settings.
+			//--------------------------------------------------------------------------------------------------------------------||
+
+			$foregroundColours = array(
+				'black'        => '0;30',
+				'dark_gray'    => '1;30',
+				'blue'         => '0;34',
+				'light_blue'   => '1;34',
+				'green'        => '0;32',
+				'light_green'  => '1;32',
+				'cyan'         => '0;36',
+				'light_cyan'   => '1;36',
+				'red'          => '0;31',
+				'light_red'    => '1;31',
+				'purple'       => '0;35',
+				'light_purple' => '1;35',
+				'brown'        => '0;33',
+				'yellow'       => '1;33',
+				'light_gray'   => '0;37',
+				'white'        => '1;37',
+			);
+
+			$backgroundColours = array(
+				'black'      => '40',
+				'red'        => '41',
+				'green'      => '42',
+				'yellow'     => '43',
+				'blue'       => '44',
+				'magenta'    => '45',
+				'cyan'       => '46',
+				'light_gray' => '47',
+			);
+
+			$colored_string = "";
+
+			// Check if given foreground color found
+			if (isset($foregroundColours[$foregroundColour])) {
+				$colored_string .= "\033[" . $foregroundColours[$foregroundColour] . "m";
+			}
+
+			// Check if given background color found
+			if (isset($backgroundColours[$backgroundColour])) {
+				$colored_string .= "\033[" . $backgroundColours[$backgroundColour] . "m";
+			}
+
+			// Add string and end coloring
+			$colored_string .=  $string . "\033[0m";
+
+			return $colored_string;
+		}
+	}
